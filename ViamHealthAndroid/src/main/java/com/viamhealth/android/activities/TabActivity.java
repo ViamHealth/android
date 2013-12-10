27b123a054526da8f08560e142a7b57dbe2ce706
Narendra Mudigal <narendra.mudigal@viamhealth.com>
2013-12-10 16:03:29 +0530

    AccountManager related changes

--- ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/TabActivity.java
+++ ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/TabActivity.java
@@ -1,22 +1,18 @@
 package com.viamhealth.android.activities;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
@@ -28,7 +24,6 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
 import com.google.analytics.tracking.android.EasyTracker;
-
 import com.viamhealth.android.Global_Application;
 import com.viamhealth.android.R;
 import com.viamhealth.android.activities.fragments.FileFragment;
@@ -49,11 +44,7 @@ import java.util.List;
 public class TabActivity extends BaseFragmentActivity implements View.OnClickListener, ActionBar.OnNavigationListener {
 
     TabHost mTabHost;
-    //TabManager mTabManager;
-    ActionBarTabManager mTabManager;
-
-    private final Map<TabTypes, SherlockFragment> mTabs = new HashMap<TabTypes, SherlockFragment>();
-    private final Map<Integer, User> usersMap = new HashMap<Integer, User>();
+    TabManager mTabManager;
 
     Animation animationMoveIn, animationMoveOut;
     FrameLayout tabContent;//, tabHeader;
@@ -64,9 +55,6 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
     final List<User> users = new ArrayList<User>();
 
     boolean headerIsVisible = true;
-
-    private User user = null;
-    private final List<User> users = new ArrayList<User>();
 
     private static final float HEADER_TOP_MARGIN_DP = 58.0f;
 
@@ -91,32 +79,9 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
 
         Actions action = (Actions) intent.getSerializableExtra("action");
 
-        user = (User) intent.getParcelableExtra("user");
-        Parcelable[] usrs = intent.getParcelableArrayExtra("users");
-        for(int i=0; i<usrs.length; i++){
-            User usr = (User)usrs[i];
-            users.add(usr);
-        }
-
         Bundle bundle = new Bundle();
         bundle.putParcelable("user", user);
         bundle.putSerializable("action", action);
-
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setLogo(R.drawable.ic_launcher);
-        actionBar.setTitle(user.getName());
-
-
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        getActionBarTab(TabTypes.Goals, bundle);
-        getActionBarTab(TabTypes.Journal, bundle);
-        getActionBarTab(TabTypes.Reminder, bundle);
-        getActionBarTab(TabTypes.Files, bundle);
-
-
         //TODO use Action Bar to create the Header
 
         /*** Action Bar Creation starts here ***/
@@ -177,20 +142,12 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
         //tabHeader = (FrameLayout) findViewById(R.id.tabHeader);
         tabs = (TabWidget) findViewById(android.R.id.tabs);
 
-
         if(action == Actions.UploadFiles){
-            //mTabHost.setCurrentTabByTag("files");
-            mTabManager.selectTab(TabTypes.Files.name());
+            mTabHost.setCurrentTabByTag("files");
             FileFragment fragment = (FileFragment) mTabManager.getCurrentSelectedTabFragment();
             fragment.pickFile();
         } else if(action == Actions.SetGoal){
-            //mTabHost.setCurrentTabByTag("goals");
-            mTabManager.selectTab(TabTypes.Goals.name());
-        }
-
-        if (savedInstanceState != null) {
-            mTabManager.selectTab(savedInstanceState.getString("tab"));
-            //mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
+            mTabHost.setCurrentTabByTag("goals");
         }
     }
 
@@ -291,116 +248,4 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
     }
 
     public enum Actions { UploadFiles, SetGoal; }
-
-    public enum TabTypes {
-        Goals(1, R.string.tab_label_goal, R.drawable.ic_action_goal, GoalFragment.class),
-        Journal(2, R.string.tab_label_journal, R.drawable.ic_action_log, JournalFragment.class),
-        Reminder(3, R.string.tab_label_reminder, R.drawable.ic_action_reminders, ReminderFragment.class),
-        Files(4, R.string.tab_label_file, R.drawable.ic_action_files, FileFragment.class);
-
-        private final int value;
-        private final int resId;
-        private final int iconId;
-        private final Class className;
-
-        TabTypes(int value, int resId, int iconId, Class className){
-            this.value = value;
-            this.resId = resId;
-            this.iconId = iconId;
-            this.className = className;
-        }
-
-
-        public int value() {return value;}
-        public int res() {return resId;}
-        public int icon() {return iconId;}
-        public Class clss() {return className;}
-
-        // Lookup table
-        private static final Map<Integer, TabTypes> valuelookup = new HashMap<Integer, TabTypes>();
-        private static final Map<Integer, TabTypes> keylookup = new HashMap<Integer, TabTypes>();
-
-        // Populate the lookup table on loading time
-        static {
-            for (TabTypes mc : EnumSet.allOf(TabTypes.class)){
-                valuelookup.put(mc.value(), mc);
-                keylookup.put(mc.res(), mc);
-            }
-        }
-
-        // This method can be used for reverse lookup purpose
-        public static TabTypes constructEnumByValue(int value) {
-            return valuelookup.get(value);
-        }
-
-        public static TabTypes constructEnumByKey(int key) {
-            return keylookup.get(key);
-        }
-
-    }
-
-
-    public class UsersActionProvider extends ActionProvider implements MenuItem.OnMenuItemClickListener {
-
-        /** Context for accessing resources. */
-        private final Context mContext;
-        private ProfilePictureView picView;
-        private final Map<Integer, User> usersMap = new HashMap<Integer, User>();
-
-        public UsersActionProvider(Context context) {
-            super(context);
-            this.mContext = context;
-        }
-
-
-        @Override
-        public View onCreateActionView() {
-            // Inflate the action view to be shown on the action bar.
-            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-            View view = layoutInflater.inflate(R.layout.menu_users, null);
-            picView = (ProfilePictureView) view.findViewById(R.id.header_user_icon);
-            if(user.getProfile()!=null)
-                picView.setProfileId(user.getProfile().getFbProfileId());
-            return view;
-        }
-
-        @Override
-        public boolean hasSubMenu() {
-            return true;
-        }
-
-        @Override
-        public boolean onPerformDefaultAction() {
-            return super.onPerformDefaultAction();
-        }
-
-        @Override
-        public void onPrepareSubMenu(SubMenu subMenu) {
-            subMenu.clear();
-
-
-        }
-
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            int userId = menuItem.getItemId();
-            if(userId==0){
-                //Logout
-                Intent returnIntent = new Intent(mContext, Home.class);
-                returnIntent.putExtra("logout", true);
-                returnIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(returnIntent);
-                return true;
-            }
-            User curUser = usersMap.get(userId);
-            Intent intent = new Intent(mContext, TabActivity.class);
-            intent.putExtra("user", user);
-            Parcelable[] usersList = new Parcelable[users.size()];
-            intent.putExtra("users", users.toArray(usersList));
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            return true;
-        }
-
-    }
 }
