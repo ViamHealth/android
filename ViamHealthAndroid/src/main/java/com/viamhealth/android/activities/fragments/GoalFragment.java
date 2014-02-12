1b854e55ad435380fe788807be15f7a3b9be3d5c
Kunal Rachhoya <kunal.rachhoya@viamhealth.com>
2014-02-12 20:56:04 +0530

    graphs performance increase and UI issues and add reading issue

--- ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/fragments/GoalFragment.java
+++ ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/fragments/GoalFragment.java
@@ -55,6 +55,8 @@ import com.viamhealth.android.utils.JsonGraphDataBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -669,6 +671,13 @@ public class GoalFragment extends BaseFragment implements View.OnClickListener {
                 }
                 rds.add(reading);
             }
+            Collections.sort(rds, new Comparator<GoalReadings>() {
+                @Override
+                public int compare(GoalReadings p1, GoalReadings p2) {
+                    return p1.getReadingDate().compareTo(p2.getReadingDate());
+                }
+
+            });
             //goal.setReadings(rds);
             goalsConfiguredMap.put(type, goal);
             return reading;
