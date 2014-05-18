d5c109e655271b7860b2ff442cf6b76837fe48c0
Kunal <kunal.rachhoya@viamhealth.com>
2014-05-18 16:01:27 +0530

    GCM - working version. not optimized.

--- ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/TabActivity.java
+++ ViamHealthAndroid/src/main/java/com/viamhealth/android/activities/TabActivity.java
@@ -1,14 +1,20 @@
 package com.viamhealth.android.activities;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
@@ -27,6 +33,9 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.viamhealth.android.Global_Application;
 import com.viamhealth.android.R;
 import com.viamhealth.android.activities.fragments.BabyGoalFragment;
@@ -35,10 +44,12 @@ import com.viamhealth.android.activities.fragments.FileFragment;
 
 import com.viamhealth.android.activities.fragments.ReminderFragmentNew;
 import com.viamhealth.android.activities.fragments.TaskScreenFragment;
+import com.viamhealth.android.dao.rest.endpoints.GCMEP;
 import com.viamhealth.android.manager.TabManager;
 import com.viamhealth.android.model.users.User;
 import com.viamhealth.android.tasks.InviteUser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,6 +76,150 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
 
     private static final float HEADER_TOP_MARGIN_DP = 58.0f;
 
+    //Move this to config
+    String SENDER_ID = "603460907161";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    GoogleCloudMessaging gcm;
+    String regid;
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("GCM", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service, if there is one.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i("GCM", "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i("GCM", "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGcmPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences(TabActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Stores the registration ID and the app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i("GCM", "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and the app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+                    storeRegistrationId(getApplicationContext(), regid);
+
+                    // You should send the registration ID to your server over HTTP, so it
+                    // can use GCM/HTTP or CCS to send messages to your app.
+                    //sendRegistrationIdToBackend();
+                    String deviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+                    GCMEP gcmep = new GCMEP(getApplicationContext(),ga);
+                    gcmep.register(deviceID, regid);
+
+                    // Persist the regID - no need to register again.
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                //mDisplay.append(msg + "\n");
+            }
+        }.execute(null, null, null);
+    }
+
+
+    // GCM ENDS
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -86,7 +241,22 @@ public class TabActivity extends BaseFragmentActivity implements View.OnClickLis
             users.add((User) pUsers[i]);
         }
 
+        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(getApplicationContext());
 
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+        } else {
+            Log.i("GCM", "No valid Google Play Services APK found.");
+        }
+
+        /*Intent intentGCM = new Intent("com.google.android.c2dm.intent.REGISTER");
+        intentGCM.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0));
+        intentGCM.putExtra("sender","603460907161");
+        startService(intentGCM);*/
 
         actionBar = getSupportActionBar();
         actionBar.setDisplayUseLogoEnabled(true);
---
+++ ViamHealthAndroid/src/main/java/com/viamhealth/android/dao/rest/endpoints/GCMEP.java
@@ -0,0 +1,69 @@
+package com.viamhealth.android.dao.rest.endpoints;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.viamhealth.android.Global_Application;
+import com.viamhealth.android.ViamHealthPrefs;
+import com.viamhealth.android.dao.restclient.core.RestClient;
+import com.viamhealth.android.dao.restclient.old.RequestMethod;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * Created by shawn on 18/5/14.
+ */
+public class GCMEP {
+
+    protected Context context;
+    protected Global_Application ga;
+    protected ViamHealthPrefs appPrefs;
+    private String tastyPieApiKey = "1111";
+
+
+    public GCMEP(Context context, Application app) {
+        this.context = context;
+        this.ga = (Global_Application)app;
+        this.appPrefs=new ViamHealthPrefs(context);
+    }
+
+    public void register(String device_id, String registration_id)  {
+        StringBuilder baseurlString = new StringBuilder(Global_Application.url)
+                .append("gcm/v1/device/register")
+                .append("/");
+
+        try {
+            JSONObject payload = new JSONObject();
+            payload.put("dev_id", device_id);
+            payload.put("reg_id", registration_id);
+            RestClient client = new RestClient(baseurlString.toString());;
+            client.AddHeader("Authorization","ApiKey " + ga.getLoggedInUser().getUsername()+":"+tastyPieApiKey);
+            client.setJSONString(payload.toString());
+            try {
+                client.Execute(RequestMethod.POST);
+                String responseString = client.getResponse();
+                Log.i("GCPEP", client.toString());
+                Log.i("GCPEP", responseString);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+        catch  (JSONException e){
+            e.printStackTrace();
+            return;
+        }
+
+
+
+
+    }
+
+    protected class Params extends HashMap<String, String> {
+
+    }
+}
