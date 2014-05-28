f968eb71905c96e1781ba39b3c0390d3591eaeef
Kunal <kunal.rachhoya@viamhealth.com>
2014-05-28 17:36:03 +0530

    misse done file

---
+++ ViamHealthAndroid/src/main/java/com/viamhealth/android/model/trackgrowth/TrackGrowthAdvData.java
@@ -0,0 +1,187 @@
+package com.viamhealth.android.model.trackgrowth;
+
+/**
+ * Created by shawn on 28/5/14.
+ */
+public class TrackGrowthAdvData {
+    private Long age = 0L;
+    private String gender;
+    private Float height_3n;
+    private Float weight_3n;
+    private Float height_2n;
+    private Float weight_2n;
+    private Float height_1n;
+    private Float weight_1n;
+    private Float height_0;
+    private Float weight_0;
+    private Float height_1;
+    private Float weight_1;
+    private Float height_2;
+    private Float weight_2;
+    private Float height_3;
+    private Float weight_3;
+
+    private Float percentile_3n = 0.3F;
+    private Float percentile_2n = 2.4F;
+    private Float percentile_1n = 16F;
+    private Float percentile_0 = 50F;
+    private Float percentile_1 = 84.1F;
+    private Float percentile_2 = 97.7F;
+    private Float percentile_3 = 99.8F;
+
+    public Float getPercentile_3n() {
+        return percentile_3n;
+    }
+
+    public Float getPercentile_2n() {
+        return percentile_2n;
+    }
+
+    public Float getPercentile_1n() {
+        return percentile_1n;
+    }
+
+    public Float getPercentile_0() {
+        return percentile_0;
+    }
+
+    public Float getPercentile_1() {
+        return percentile_1;
+    }
+
+    public Float getPercentile_2() {
+        return percentile_2;
+    }
+
+    public Float getPercentile_3() {
+        return percentile_3;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Float getHeight_3n() {
+        return height_3n;
+    }
+
+    public void setHeight_3n(Float height_3n) {
+        this.height_3n = height_3n;
+    }
+
+    public Float getWeight_3n() {
+        return weight_3n;
+    }
+
+    public void setWeight_3n(Float weight_3n) {
+        this.weight_3n = weight_3n;
+    }
+
+    public Float getHeight_2n() {
+        return height_2n;
+    }
+
+    public void setHeight_2n(Float height_2n) {
+        this.height_2n = height_2n;
+    }
+
+    public Float getWeight_2n() {
+        return weight_2n;
+    }
+
+    public void setWeight_2n(Float weight_2n) {
+        this.weight_2n = weight_2n;
+    }
+
+    public Float getHeight_1n() {
+        return height_1n;
+    }
+
+    public void setHeight_1n(Float height_1n) {
+        this.height_1n = height_1n;
+    }
+
+    public Float getWeight_1n() {
+        return weight_1n;
+    }
+
+    public void setWeight_1n(Float weight_1n) {
+        this.weight_1n = weight_1n;
+    }
+
+    public Float getHeight_0() {
+        return height_0;
+    }
+
+    public void setHeight_0(Float height_0) {
+        this.height_0 = height_0;
+    }
+
+    public Float getWeight_0() {
+        return weight_0;
+    }
+
+    public void setWeight_0(Float weight_0) {
+        this.weight_0 = weight_0;
+    }
+
+    public Float getHeight_1() {
+        return height_1;
+    }
+
+    public void setHeight_1(Float height_1) {
+        this.height_1 = height_1;
+    }
+
+    public Float getWeight_1() {
+        return weight_1;
+    }
+
+    public void setWeight_1(Float weight_1) {
+        this.weight_1 = weight_1;
+    }
+
+    public Float getHeight_2() {
+        return height_2;
+    }
+
+    public void setHeight_2(Float height_2) {
+        this.height_2 = height_2;
+    }
+
+    public Float getWeight_2() {
+        return weight_2;
+    }
+
+    public void setWeight_2(Float weight_2) {
+        this.weight_2 = weight_2;
+    }
+
+    public Float getHeight_3() {
+        return height_3;
+    }
+
+    public void setHeight_3(Float height_3) {
+        this.height_3 = height_3;
+    }
+
+    public Float getWeight_3() {
+        return weight_3;
+    }
+
+    public void setWeight_3(Float weight_3) {
+        this.weight_3 = weight_3;
+    }
+}
